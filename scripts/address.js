7eaa0417b8d2ec16d3a4b7aa8a87f03e5648d8de
Camila Figueiredo <camila_fig@hotmail.com>
2024-04-01 22:41:56 -0300

    some corrections

--- scripts/address.js
+++ scripts/address.js
@@ -1,9 +1,11 @@
+//Para escrever no campo "Insira seu endereço" a rua e o número de onde será a entrega
+
 const address = document.getElementById("address")
 const addressData = []
 
 function save_address(){
     const infoRua = prompt("Onde você quer receber seu pedido?")
-    const infoNum = prompt("Informe o número do enedereço")
+    const infoNum = prompt("Informe o número do endereço")
 
     addressData.push({
         street: infoRua,
--- scripts/order.js
+++ scripts/order.js
@@ -18,7 +18,7 @@ let bacon = {
     preçoOriginal: 24.90,
 }
 
-let queijoQuente = {
+let queijo = {
     nome: "Queijo Quente",
     preçoDesconto: 10.90,
     preçoOriginal: 13.90,
@@ -30,10 +30,19 @@ let rosbife = {
     preçoOriginal: 24.90,
 }
 
-const opções = [italiano, grelhado, bacon, queijoQuente, rosbife] // Array de todas as opções de itens para comparação com o selecionado nos prompts
+let croissant = {
+    nome: "Croissant",
+    preçoDesconto: 19.90,
+    preçoOriginal: 24.90,
+}
+
+const opções = ['italiano', 'grelhado', 'bacon', 'queijo', 'rosbife', 'croissant'] // Array de todas as opções de itens para comparação com o selecionado nos prompts
 const sacola = [] // Array da sacola, itens adicionados entram aqui
 
-const addGeral = (addGeral) => { // Botão de adicionar, prompt pede pro cliente digitar o nome do item e a quantidade que deseja adicionar e inclui isso no array da Sacola
+
+// Botão de adicionar, prompt pede pro cliente digitar o nome do item e a quantidade que deseja adicionar e inclui isso no array da Sacola
+// Estamos pensando em excluir este botão
+const addGeral = (addGeral) => { 
     const addItemGeral = prompt ('Qual item você deseja adicionar à sacola?')
     const quantItemGeral = parseInt (prompt ('Quantas unidades você deseja adicionar à sacola?'))
     if (opções.includes(addItemGeral) && quantItemGeral == 1) { // Retorna o item adicionado no singular
@@ -53,7 +62,8 @@ const addGeral = (addGeral) => { // Botão de adicionar, prompt pede pro cliente
 }
 
 
-const addIndividual = (item) => { // Botão de adicionar dentro de cada item, prompt pede a quantidade e adiciona no array da Sacola
+// Botão de adicionar dentro de cada item, prompt pede a quantidade e adiciona no array da Sacola
+const addIndividual = (item) => { 
     const quantItemIndividual = parseInt (prompt ('Quantas unidades você deseja adicionar à sacola?'))
     if (quantItemIndividual == 1) { // Retorna o item adicionado no singular
         for (let i = 0; i < quantItemIndividual; i++)
@@ -67,13 +77,21 @@ const addIndividual = (item) => { // Botão de adicionar dentro de cada item, pr
     else if (quantItemIndividual == 0) { // Retorna erro caso a quantidade seja 0
         return alert (`Insira uma quantidade válida.`)
     }
-}
-
-const verSacola = () => {} // Alert indica todos os itens que estão na sacola, seus vsalores e o preço total do pedido até o momento
-//map 
-
-const avaliação = () => {} // Prompt pede uma nota de 1 a 5, calcula a média das notas e quantidade de avaliações e atualiza a nota em tempo real
 
 
+ //Para escrever a quantidade total de itens e o valor total ao lado do desenho da sacola no header
+const saveItem = document.getElementById("header-bag__itens")
+const saveValue = document.getElementById("header-bag__cost")
 
-//salvar infos local storage
+const totalSacola = sacola.reduce ((soma, item) => soma + item.preçoDesconto, 0)
+const totalSacolaDuasCasas = totalSacola.toFixed(2).toString().replace(".", ",") 
+
+saveValue.innerHTML = `R$ ${totalSacolaDuasCasas}`
+
+const quantidadeItens = sacola.length
+sacola.map ((bag) => {
+    if (quantidadeItens > 1) {
+        saveItem.innerHTML = `${quantidadeItens} itens`
+    }
+    else {saveItem.innerHTML = `${quantidadeItens} item`}
+})}
