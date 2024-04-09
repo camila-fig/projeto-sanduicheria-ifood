385a5bf22b3fe72165de43fc8a7baf7b3547b0b4
Camila <camila_fig@hotmail.com>
2024-04-08 21:04:58 -0300

    saving LocalStorage

--- scripts/address.js
+++ scripts/address.js
@@ -3,6 +3,13 @@
 const address = document.getElementById("address")
 const addressData = []
 
+const infoLocalStorage = JSON.parse(localStorage.getItem('addressData'))
+
+infoLocalStorage.map((item) => {
+    address.innerHTML = `${item.street}, ${item.number}`
+        })           
+
+
 function save_address(){
     const infoRua = prompt("Em qual rua/avenida você quer receber seu pedido?")
     const infoNum = prompt("Informe o número do endereço")
@@ -19,33 +26,3 @@ function save_address(){
     address.innerHTML = `${item.street}, ${item.number}`
         })           
     }
-
-
-    
-// const address = document.getElementById("address")
-// const addressData = []
-
-
-
-// function save_address(){
-
-// const endereco= JSON.parse(localStorage.getItem("addressData"))
-
-// if (endereco){
-//     endereco.map((item) => {
-//     address.innerHTML = `${item.street}, ${item.number}`
-// }) else {
-//         const infoRua = prompt("Em qual rua/avenida você quer receber seu pedido?")
-//         const infoNum = prompt("Informe o número do endereço")
-    
-//         addressData.push({
-//             street: infoRua,
-//             number: infoNum
-//         })
-
-//         localStorage.setItem("addressData", JSON.stringify(addressData))
-//         const endereco = JSON.parse(localStorage.getItem("addressData"))
-//         endereco.map((item) => {
-//         address.innerHTML = `${item.street}, ${item.number}`
-//     }
-// }}
