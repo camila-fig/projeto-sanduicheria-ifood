5b180d1019d1926dd2e4d73aad44475bf1e3ca7c
Camila Figueiredo <camila_fig@hotmail.com>
2024-04-11 20:58:52 -0300

    address saved in localStorage

--- scripts/address.js
+++ scripts/address.js
@@ -5,11 +5,12 @@ const addressData = []
 
 const infoLocalStorage = JSON.parse(localStorage.getItem('addressData'))
 
+//Caso tenha um endereço salvo no LocalStorage, vai carregar automático
 infoLocalStorage.map((item) => {
     address.innerHTML = `${item.street}, ${item.number}`
         })           
 
-
+//Caso não tenha nenhum endereço, vai solicitar
 function save_address(){
     const infoRua = prompt("Em qual rua/avenida você quer receber seu pedido?")
     const infoNum = prompt("Informe o número do endereço")
