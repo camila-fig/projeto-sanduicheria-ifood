7f4756be59231d5810c3a1592775e1ca5398a117
Camila Figueiredo <camila_fig@hotmail.com>
2024-04-17 21:46:53 -0300

    Valores do modal

--- scripts/order.js
+++ scripts/order.js
@@ -51,94 +51,93 @@ function closeModal() {
 
 
 // Botão de adicionar dentro de cada item, prompt pede a quantidade e adiciona no array da Sacola
-const addIndividual = (item) => { 
-    const quantItemIndividual = parseInt (prompt ('Quantas unidades você deseja adicionar à sacola?'))
+const addIndividual = (item) => {
+    const quantItemIndividual = parseInt(prompt('Quantas unidades você deseja adicionar à sacola?'))
     if (quantItemIndividual == 1) { // Retorna o item adicionado no singular
         for (let i = 0; i < quantItemIndividual; i++)
-        sacola.push(item)
-        alert (`O item ${item.nome} foi adicionado à sua sacola`)
+            sacola.push(item)
+        alert(`O item ${item.nome} foi adicionado à sua sacola`)
     } else if (quantItemIndividual > 1) { // Retorna o item adicionado no plural
         for (let i = 0; i < quantItemIndividual; i++)
-        sacola.push(item)
-        alert (`O item ${item.nome} foi adicionado ${quantItemIndividual} vezes à sua sacola`)
+            sacola.push(item)
+        alert(`O item ${item.nome} foi adicionado ${quantItemIndividual} vezes à sua sacola`)
     }
     else if (quantItemIndividual == 0) { // Retorna erro caso a quantidade seja 0
-        return alert (`Insira uma quantidade válida.`)
+        return alert(`Insira uma quantidade válida.`)
     }
 
 
- //Para escrever a quantidade total de itens e o valor total ao lado do desenho da sacola no cabeçalho
-const saveItem = document.getElementById("header-bag__itens")
-const saveValue = document.getElementById("header-bag__cost")
+    //Salvando a sacola dentro do LocalStorage
+    localStorage.setItem("pedido", JSON.stringify(sacola))
+    const InfoLocalStorageSacola = JSON.parse(localStorage.getItem("pedido"))
 
-const totalSacola = sacola.reduce ((soma, item) => soma + item.preçoDesconto, 0)
-const totalSacolaDuasCasas = totalSacola.toFixed(2).toString().replace(".", ",") 
 
-saveValue.innerHTML = `R$ ${totalSacolaDuasCasas}`
+    //Constantes de dentro do modal
+    const orderName = document.getElementById('order-name')
+    const orderValue = document.getElementById('order-value')
+    const subtotal = document.getElementById('sub__valor')
+    const total = document.getElementById('total__valor')
+    const saveItem = document.getElementById("header-bag__itens")
+    const saveValue = document.getElementById("header-bag__cost")
+    const criaNome = document.createElement('p')
+    const criaValor = document.createElement('p')
 
-const quantidadeItens = sacola.length
-sacola.map ((bag) => {
-    if (quantidadeItens > 1) {
-        saveItem.innerHTML = `${quantidadeItens} itens`
+
+    //Para escrever a quantidade total de itens e o valor total ao lado do desenho da sacola no cabeçalho dentro do modal
+    const totalSacola = InfoLocalStorageSacola.reduce((soma, item) => soma + item.preçoDesconto, 0)
+    const totalSacolaDuasCasas = totalSacola.toFixed(2).toString().replace(".", ",")
+
+    saveValue.innerHTML = `R$ ${totalSacolaDuasCasas}`
+
+    const quantidadeItens = InfoLocalStorageSacola.length
+    InfoLocalStorageSacola.map((bag) => {
+        if (quantidadeItens > 1) {
+            saveItem.innerHTML = `${quantidadeItens} itens`
+        }
+        else { saveItem.innerHTML = `${quantidadeItens} item` }
+    })
+
+
+    //Para armazenar o pedido com quantidades (agrupados) e multiplicar os valores parciais
+    const cont = []
+    let totalCont = 1
+
+    for (let i = 0; i < InfoLocalStorageSacola.length; i++) {
+        const nomeItem = InfoLocalStorageSacola[i].nome
+        if (i < InfoLocalStorageSacola.length - 1 && nomeItem == InfoLocalStorageSacola[i + 1].nome) {
+            totalCont++
+        } else {
+            cont.push({nome: nomeItem, total: totalCont})
+            totalCont = 1
+        }
+
+        localStorage.setItem("quantidades", JSON.stringify(cont))
+        const InfoLocalStorageCont = JSON.parse(localStorage.getItem("quantidades"))
+
+        InfoLocalStorageCont.map((cont) => {
+            criaNome.innerHTML = `${cont.total}x ${InfoLocalStorageSacola[i].nome}`
+            orderName.append(criaNome)
+
+            InfoLocalStorageSacola.map((item) => {
+                const precoDescontoMultiplicado = item.preçoDesconto * cont.total
+                const precoDesconto = precoDescontoMultiplicado.toFixed(2).toString().replace(".", ",")
+                criaValor.innerHTML = `R$ ${precoDesconto}`
+                orderValue.append(criaValor)
+            })
+        })
     }
-    else {saveItem.innerHTML = `${quantidadeItens} item`}
-})
 
 
-//Comandos de dentro do modal
-const orderName = document.getElementById('order-name')
-const orderValue = document.getElementById('order-value')
-const subtotal = document.getElementById('sub__valor')
-const total = document.getElementById('total__valor')
-const criaNome = document.createElement('p') 
-const criaValor = document.createElement('p')
+    //Para escrever o valor total dentro do modal
+    subtotal.innerHTML = totalSacolaDuasCasas
 
-//Para armazenar os preços por item
-localStorage.setItem("pedido", JSON.stringify(sacola))
-const InfoLocalStorageSacola = JSON.parse(localStorage.getItem("pedido"))
 
-InfoLocalStorageSacola.map((item) => {
-    const precoDesconto = item.preçoDesconto.toFixed(2).toString().replace(".", ",")
-    criaValor.innerHTML = `R$ ${precoDesconto}`
-    orderValue.append(criaValor)
-})
-
-//Para armazenar o pedido com quantidades (agrupados)
-const cont = []
-let totalCont = 1;
-for (let i = 0; i < sacola.length; i++) {
-    const nomeItem = sacola[i].nome
-    if (i < sacola.length - 1 && nomeItem == sacola[i + 1].nome) {
-        totalCont++
-    } else {            
-        cont.push({nome: nomeItem, total: totalCont})
-        totalCont = 1       
+    //Para escrever o valor total somado com frete dentro do modal
+    function totalComTaxa(totalSacola, taxa) {
+        return totalSacola + taxa
     }
-    
-localStorage.setItem("quantidades", JSON.stringify(cont))
-const InfoLocalStorageCont = JSON.parse(localStorage.getItem("quantidades"))
-
-InfoLocalStorageCont.map((item) =>{
-    criaNome.innerHTML = `${item.total}x ${sacola[i].nome}`
-    orderName.append(criaNome)
-})        
-    }
-
-             
-//Para escrever o valor total dentro do modal
-subtotal.innerHTML = totalSacolaDuasCasas
-
-//Para escrever o valor total somado com frete dentro do modal
-function totalComTaxa (totalSacola, taxa) {
-    return totalSacola + taxa;
-}
-let resultadoTotalTaxa = totalComTaxa(totalSacola, 9.9)
-total.innerHTML = resultadoTotalTaxa.toFixed(2).toString().replace(".", ",")
-
-
-
-console.log(InfoLocalStorageSacola)
-
+    let resultadoTotalTaxa = totalComTaxa(totalSacola, 9.9)
+    total.innerHTML = resultadoTotalTaxa.toFixed(2).toString().replace(".", ",")
 }
 
 
